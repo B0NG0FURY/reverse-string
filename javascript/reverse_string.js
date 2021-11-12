57ec348c8ad459f97e249a03a85a3d7f1644f92e
bongofury <jtroutmanman@gmail.com>
2021-11-12 10:26:18 -0600

    completed javascript solution.

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,11 +1,19 @@
 function reverseString(str) {
-  // type your code here
+  const length = str.length;
+  const result = [];
+  for (let i = length; i > -1; i--) {
+    result.push(str[i]);
+  }
+  return result.join("");
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString("hi"));
+
+  console.log("Expecting: 'ecin'");
+  console.log("Result: ", reverseString("nice"));
 
   console.log("");
 
@@ -16,4 +24,9 @@ if (require.main === module) {
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+
+// 1. Grab the length of string and assign it to a variable.
+// 2. Create an empty Array call result.
+// 3. Iterate over the string starting at string length - 1 and then decreasing the iterator by 1 until iterator equals 0. With each iteration, push the current value into the result Array.
+// 4. Once iteration is done, join the result array together.
 // And a written explanation of your solution
